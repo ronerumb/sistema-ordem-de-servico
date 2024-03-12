fa24ef729c4fa99a707ee17262fa7fbdc916fc1f
Roner <ronerumb@gmail.com>
2024-03-12 16:56:51 -0300

    finalizacao do crud service

--- src/modules/Services/controller/servicesController.ts
+++ src/modules/Services/controller/servicesController.ts
@@ -30,10 +30,72 @@ class ServicesController{
                 data : await servicesService.create(name,description,price),
             })
             
-        } catch (error) {
+        } catch (err:any) {
+            return res.status(400).json({
+                error: err.message,
+            })
             
         }
 
         
+    }
+
+        public async update(req:Request, res: Response){
+        const {name,description,price} = req.body;
+
+        try {
+            const validator = z.object({
+                name : z.string().min(1,{message : 'Nome é obrigatorio'}),  
+                email : z.string().min(1,{message : 'Descrição é obrigatorio'}) 
+               
+            });
+            validator.parse({name,description});
+            
+        } catch (err:any) {
+            return res.status(400).json({
+                message : err.errors
+            })
+            
+        }
+
+        try {
+            return res.json({
+                message : 'Atualizado com sucesso',
+                data : await servicesService.update(name,description,price),
+            })
+            
+        } catch (err:any) {
+            return res.status(400).json({
+                message : err.errors
+            })
+        }
+
+        
+    }
+
+    public async delete (req:Request, res:Response){
+        const name = req.body.email;
+
+        try {
+            const validator = z.string().min(1,{message: 'Email é obrigatorio'} )
+            validator.parse(name);
+            
+        } catch (err:any) {
+            res.status(400).json({
+                error:err.message,
+            })
+            
+        }
+
+        try {
+            res.status(200).json({
+                message: 'Excluido com sucesso',
+                date: await servicesService.delete(name),
+            })
+        } catch (err:any) {
+            return res.status(400).json({
+                message : err.errors
+            })
+        }
     }
 }
--- src/modules/Services/service/servicesService.ts
+++ src/modules/Services/service/servicesService.ts
@@ -34,6 +34,56 @@ class ServicesService{
             
     }
 
+    public async update(name: string,description: string,price:number){
+        const findService = await prismaConnect.services.findMany({
+            where: {
+                name,
+            }
+        });
+
+        if (findService){
+            throw new Error('Serviço não encontrado')
+        }
+
+        const update = await prismaConnect.services.update({
+            where: {
+                name,
+            },
+            data: {
+                name: name,
+                description :description,
+                price : price
+                
+            }
+        })
+
+        return update;
+
+       
+
+            
+    }
+
+    public async delete( name: string) {
+        const findService = await prismaConnect.services.findUnique({
+            where : {
+                name,
+            }
+        })
+
+        if (!findService) {
+            throw new Error('Serviço não cadastrado')
+
+        }
+
+        const delet = await prismaConnect.services.delete({
+            where:{
+                name,
+            }
+        })
+        return delet;
+    }
+
 }
 
 export const servicesService = new ServicesService();
